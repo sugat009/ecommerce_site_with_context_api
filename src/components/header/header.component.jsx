e026ea4b81d02919b76ce1dbc47137ffe2162ab9
Sugat Bajracharya <sugatbajracharya49@gmail.com>
2020-04-12 12:10:42 +0545

    Cart Icon toggle feature moved from redux to context API

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {createStructuredSelector} from 'reselect';
@@ -7,14 +7,18 @@ import {auth} from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import {selectCartHidden} from '../../redux/cart/cart.selectors';
+import CurrentUserContext from "../../contexts/current-user/current-user.context";
 
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 
 import './header.styles.scss';
-import CurrentUserContext from "../../contexts/current-user/current-user.context";
+import CartContext from "../../contexts/cart/cart.context";
 
-const Header = ({hidden}) => {
+const Header = () => {
     const currentUser = useContext(CurrentUserContext);
+    const [hidden, setHidden] = useState(true);
+    const toggleHidden = () => setHidden(!hidden);
+
     return (
         <div className='header'>
             <Link
@@ -51,7 +55,9 @@ const Header = ({hidden}) => {
                         SIGN IN
                     </Link>
                 )}
-                <CartIcon/>
+                <CartContext.Provider value={{hidden, toggleHidden}}>
+                    <CartIcon/>
+                </CartContext.Provider>
             </div>
             {hidden ? null : <CartDropdown/>}
         </div>
