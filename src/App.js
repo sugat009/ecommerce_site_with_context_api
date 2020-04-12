d7bc1781a675fc942f2158a63cda7b2380bc7e27
Sugat Bajracharya <sugatbajracharya49@gmail.com>
2020-04-12 11:34:25 +0545

    Current user changed to context API, changes in App component and Header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import {Redirect, Route, Switch} from 'react-router-dom';
-import {connect} from 'react-redux';
-import {createStructuredSelector} from 'reselect';
 
 import './App.css';
 
@@ -13,29 +11,33 @@ import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component';
 
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
-
-import {setCurrentUser} from './redux/user/user.actions';
-import {selectCurrentUser} from './redux/user/user.selectors';
+import CurrentUserContext from "./contexts/current-user/current-user.context";
 
 class App extends React.Component {
     unsubscribeFromAuth = null;
 
-    componentDidMount() {
-        const {setCurrentUser} = this.props;
+    constructor() {
+        super();
 
+        this.state = {
+            currentUser: null
+        };
+    }
+
+    componentDidMount() {
         this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
             if (userAuth) {
                 const userRef = await createUserProfileDocument(userAuth);
 
                 userRef.onSnapshot(snapShot => {
-                    setCurrentUser({
+                    this.setState({
                         id: snapShot.id,
                         ...snapShot.data()
                     });
                 });
             }
 
-            setCurrentUser(userAuth);
+            this.setState({currentUser: userAuth});
         });
     }
 
@@ -46,7 +48,9 @@ class App extends React.Component {
     render() {
         return (
             <div>
-                <Header/>
+                <CurrentUserContext.Provider value={this.state.currentUser}>
+                    <Header/>
+                </CurrentUserContext.Provider>
                 <Switch>
                     <Route
                         exact
@@ -66,7 +70,7 @@ class App extends React.Component {
                         exact
                         path='/signin'
                         render={() =>
-                            this.props.currentUser ? (
+                            this.state.currentUser ? (
                                 <Redirect to='/'/>
                             ) : (
                                 <SignInAndSignUpPage/>
@@ -79,15 +83,4 @@ class App extends React.Component {
     }
 }
 
-const mapStateToProps = createStructuredSelector({
-    currentUser: selectCurrentUser
-});
-
-const mapDispatchToProps = dispatch => ({
-    setCurrentUser: user => dispatch(setCurrentUser(user))
-});
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(App);
+export default App;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,48 +1,65 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import React, {useContext} from 'react';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {createStructuredSelector} from 'reselect';
 
-import { auth } from '../../firebase/firebase.utils';
+import {auth} from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
-import { selectCartHidden } from '../../redux/cart/cart.selectors';
-import { selectCurrentUser } from '../../redux/user/user.selectors';
+import {selectCartHidden} from '../../redux/cart/cart.selectors';
 
-import { ReactComponent as Logo } from '../../assets/crown.svg';
+import {ReactComponent as Logo} from '../../assets/crown.svg';
 
 import './header.styles.scss';
+import CurrentUserContext from "../../contexts/current-user/current-user.context";
 
-const Header = ({ currentUser, hidden }) => (
-  <div className='header'>
-    <Link className='logo-container' to='/'>
-      <Logo className='logo' />
-    </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>
-        SHOP
-      </Link>
-      <Link className='option' to='/shop'>
-        CONTACT
-      </Link>
-      {currentUser ? (
-        <div className='option' onClick={() => auth.signOut()}>
-          SIGN OUT
+const Header = ({hidden}) => {
+    const currentUser = useContext(CurrentUserContext);
+    return (
+        <div className='header'>
+            <Link
+                className='logo-container'
+                to='/'
+            >
+                <Logo className='logo'/>
+            </Link>
+            <div className='options'>
+                <Link
+                    className='option'
+                    to='/shop'
+                >
+                    SHOP
+                </Link>
+                <Link
+                    className='option'
+                    to='/shop'
+                >
+                    CONTACT
+                </Link>
+                {currentUser ? (
+                    <div
+                        className='option'
+                        onClick={() => auth.signOut()}
+                    >
+                        SIGN OUT
+                    </div>
+                ) : (
+                    <Link
+                        className='option'
+                        to='/signin'
+                    >
+                        SIGN IN
+                    </Link>
+                )}
+                <CartIcon/>
+            </div>
+            {hidden ? null : <CartDropdown/>}
         </div>
-      ) : (
-        <Link className='option' to='/signin'>
-          SIGN IN
-        </Link>
-      )}
-      <CartIcon />
-    </div>
-    {hidden ? null : <CartDropdown />}
-  </div>
-);
+    )
+};
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser,
-  hidden: selectCartHidden
+    hidden: selectCartHidden
 });
 
 export default connect(mapStateToProps)(Header);
